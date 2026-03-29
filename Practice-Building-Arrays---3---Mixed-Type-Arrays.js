0887b9913d7e3735b7ab198677fe21770de4b8d9
haasjohn <Jdhaas2014@gmail.com>
2026-03-28 22:13:11 -0500

    JDH 3/28/2026

---
+++ Practice-Building-Arrays---3---Mixed-Type-Arrays.js
@@ -0,0 +1,28 @@
+//Practice: Building Arrays - 3 - Mixed-Type Arrays
+
+let user = {
+  userName: "Mutz",
+  age: 56,
+  isSubscribed: true,
+  location: "Illinois",
+  hobbies: ["drinking", "fighting"]
+};
+
+// Display the profile
+console.log(user);
+
+// Display individual details
+console.log("Name:", user.userName);
+console.log("Age:", user.age);
+console.log("Subscribed:", user.isSubscribed);
+console.log("Location:", user.location);
+console.log("Hobbies:", user.hobbies);
+
+console.log(user.userName);  
+console.log(user.hobbies[1]);   // hobby #2
+
+user.age = 35;                  // update age
+user.hobbies.push("breaking things");   // add new hobby
+
+console.log(user);
+
